0b5bb49fc9034e97c215a0a443ff8a181aab4be6
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-14 23:47:47 +0200

    Update deploy script

--- evemonk/deploy.sh
+++ evemonk/deploy.sh
@@ -2,9 +2,15 @@
 
 set -eux
 
+git stash
+git pull
+git stash pop
+
 docker-compose pull
 docker-compose down
 systemctl stop docker
 systemctl start docker
 docker-compose up -d
+
+docker system prune --all
 
